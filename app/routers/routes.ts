5da755ccca740088284d20a3e210d2be67359043
Stephanie.Gao <tm1997@163.com>
2021-07-17 17:05:57 +0800

    feat(original-webpack): add demo - sierpinski 3d (points)

--- app/routers/routes.ts
+++ app/routers/routes.ts
@@ -92,6 +92,11 @@ const pcRoutes: IRouteConfig[] = [
             link: '/original-webgl/sierpinski-2d-lines',
             component: (): ILoadComponent => import('src/components/original-webgl/sierpinski/sierpinski-2d-lines'),
           },
+          {
+            title: 'Sierpinski 3d (points)',
+            link: '/original-webgl/sierpinski-3d-points',
+            component: (): ILoadComponent => import('src/components/original-webgl/sierpinski/sierpinski-3d-points'),
+          },
         ],
       },
     ],
