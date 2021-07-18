1d0baf84e179a8781b77e1325ef8ed5f4e474548
Stephanie.Gao <tm1997@163.com>
2021-07-18 23:10:51 +0800

    feat(original-webpack): update demo - add interactive input

--- src/components/original-webgl/rotating-square/index.tsx
+++ src/components/original-webgl/rotating-square/index.tsx
@@ -1,9 +1,11 @@
-import React, { useCallback, useEffect, useRef } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { Radio, Slider, InputNumber, Col, Row } from 'antd';
 import { useShaders, useWebGL, useRender } from 'src/components/original-webgl/hooks';
 import { flatten, vec2 } from 'src/lib/mvjs';
 import vShader from './shaders/vshader.glsl';
 import fShader from './shaders/fshader.glsl';
 
+const { Group: RadioGroup, Button: RadioButton } = Radio;
 const vertices = [
   vec2( 0,  1),
   vec2( 1,  0),
@@ -18,16 +20,18 @@ export default function Comp(): JSX.Element {
   const theta = useRef(0);
   // 在renderFunc中频繁用到thetaLoc，故存放在ref中
   const thetaLoc = useRef<WebGLUniformLocation|null>(null);
+  const [direction, setDirection] = useState<boolean>(true);
+  const [speed, setSpeed] = useState<number>(5);
   const renderFunc = useCallback(() => {
     if (gl && program && thetaLoc.current) {
       gl.clear(gl.COLOR_BUFFER_BIT);
 
-      theta.current += 0.01;
+      theta.current += (direction ? 0.01 : -0.01) * speed;
       gl.uniform1f(thetaLoc.current, theta.current);
   
       gl.drawArrays(gl.TRIANGLE_STRIP, 0, vertices.length);
     }
-  }, [gl, program]);
+  }, [gl, program, direction, speed]);
 
   useEffect(() => {
     if (gl && canvasRef.current && program) {
@@ -52,6 +56,36 @@ export default function Comp(): JSX.Element {
   return (
     <div style={{ padding: '20px' }}>
       <canvas ref={canvasRef} width="512" height="512"></canvas>
+      <Row gutter={10}>
+        <Col style={{ marginRight: '20px' }}>Direction: </Col>
+        <Col span={8}>
+          <RadioGroup value={direction} onChange={(e) => setDirection(e.target.value)} size="small">
+            <RadioButton value={false}>clockwise</RadioButton>
+            <RadioButton value={true}>counter-clockwise</RadioButton>
+          </RadioGroup>
+        </Col>
+      </Row>
+      <Row gutter={10}>
+        <Col style={{ marginRight: '20px' }}>Speed: </Col>
+        <Col style={{ width: '300px' }}>
+          <Slider
+            min={0}
+            max={10}
+            step={0.01}
+            value={speed}
+            onChange={(val: number) => setSpeed(val)}
+          />
+        </Col>
+        <Col span={4}>
+          <InputNumber
+            min={0}
+            max={10}
+            step={0.01}
+            value={speed}
+            onChange={(val) => setSpeed(val)}
+          />
+        </Col>
+      </Row>
     </div>
   );
 }
