7d22a384d9e6d76c10604349f56f853e5613f0c0
Stephanie.Gao <tm1997@163.com>
2021-08-29 23:20:09 +0800

    feat(mvjs): add vec-funcs - negate, dot, cross, length and normalize

--- src/lib/mvjs/lib/vec-funcs.ts
+++ src/lib/mvjs/lib/vec-funcs.ts
@@ -1,4 +1,4 @@
-import { IVec } from './vector';
+import { IVec, IVec3, vec3 } from './vector';
 
 /**
  * 根据指定的倍率缩放向量
@@ -37,4 +37,98 @@ export function mix<T extends IVec>(u: T, v: T, s: number): T {
   }
 
   return result as T;
+}
+
+/**
+ * 求向量的非
+ * @param u 向量
+ * @returns 向量的非
+ */
+export function negate<T extends IVec>(u: T): T {
+  const result = [];
+  for (let i = 0; i < u.length; i += 1) {
+    result.push( -u[i] );
+  }
+
+  return result as T;
+}
+
+/**
+ * 求向量的点积
+ * @param u 向量u
+ * @param v 向量v
+ * @returns 向量的点积
+ */
+export function dot<T extends IVec>(u: T, v: T): number {
+  if (u.length !== v.length) {
+    throw new Error('dot(): vectors are not the same dimension');
+  }
+
+  let sum = 0;
+  for (let i = 0; i < u.length; i += 1) {
+    sum += u[i] * v[i];
+  }
+
+  return sum;
+}
+
+/**
+ * 求向量的叉积
+ * @param u 向量u
+ * @param v 向量v
+ * @returns 向量的叉积
+ */
+export function cross(u: IVec3, v: IVec3): IVec3 {
+  if (!Array.isArray(u) || u.length < 3) {
+    throw new Error('cross(): first argument is not a vector of at least 3');
+  }
+
+  if (!Array.isArray(v) || v.length < 3) {
+    throw new Error('cross(): second argument is not a vector of at least 3');
+  }
+
+  const result = vec3(
+    u[1] * v[2] - u[2] * v[1],
+    u[2] * v[0] - u[0] * v[2],
+    u[0] * v[1] - u[1] * v[0]
+  );
+
+  return result;
+}
+
+/**
+ * 求向量的长度(几何中的向量长度)
+ * @param u 向量
+ * @returns 向量的长度
+ */
+export function length<T extends IVec>(u: T): number {
+  return Math.sqrt( dot(u, u) );
+}
+
+/**
+ * 求向量的标准化结果
+ * @param u 向量
+ * @param excludeLastItem 是否忽略最后一个元素
+ * @returns 标准化后的向量
+ */
+export function normalize<T extends IVec>(u: T, excludeLastItem = false): T {
+  let last;
+  if (excludeLastItem) {
+    last = u.pop();
+  }
+
+  const len = length(u);
+  if (!isFinite(len)) {
+    throw new Error('normalize(): vector has zero length')
+  }
+
+  for (let i = 0; i < u.length; i += 1) {
+    u[i] /= len;
+  }
+
+  if (excludeLastItem && last !== undefined) {
+    u.push(last);
+  }
+
+  return u;
 }
